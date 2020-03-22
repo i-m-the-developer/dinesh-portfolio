0a6ce533bb59ea4dc9697b5ebaa1d7fcbe6e9faf
Dinesh Thapa <dinesh-thapa@hotmail.com>
2020-03-22 17:09:50 -0400

    inital commit

---
+++ src/components/about.jsx
@@ -0,0 +1,84 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-about" data-section="about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-12">
+                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                <div className="col-md-12">
+                    <div className="about-desc">
+                    <span className="heading-meta">About Us</span>
+                    <h2 className="colorlib-heading">Who Am I?</h2>
+                    <p>I am a Full Stack Software Engineer with the UI background of HTML, CSS, JS, JavaScript, Jquery, Angular, React, Primefaces, Spring MVC, JSF MVC for UI and Java, Spring Boot, Ibatis, HIbernate, Spring WebFlow, for Backend.</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+        <section className="colorlib-about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">What I do?</span>
+                <h2 className="colorlib-heading">Here are some of my expertise</h2>
+            </div>
+            </div>
+            <div className="row row-pt-md">
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-1">
+                <span className="icon">
+                    <i className="icon-bulb" />
+                </span>
+                <div className="desc">
+                    <h3>Web Development </h3>
+                    <p>I have experience building UI for web application using JavaScript, React, Angular, Primefaces, HTML, CSS</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-3">
+                <span className="icon">
+                    <i className="icon-bulb" />
+                </span>
+                <div className="desc">
+                    <h3>Java</h3>
+                    <p>I have a good understanding of Core Java OOPs Concepts.</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-3">
+                <span className="icon">
+                    <i className="icon-bulb" />
+                </span>
+                <div className="desc">
+                    <h3>Spring</h3>
+                    <p>I have a good understanding of Spring Framwork: Spring MVC, Spring Boot, Spring WebFlow, Spring Security.</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-5">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>Oracle</h3>
+                    <p>I have solid knowledge in Oracle relational database.</p>
+                </div>
+                </div>
+            </div>
+            
+            </div>
+        </div>
+        </section>
+      </div>
+    )
+  }
+}
---
+++ src/components/timeline.jsx
@@ -0,0 +1,284 @@
+import React, { Component } from "react";
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div
+                className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Web Engineer II at Converge-An Arrow Company{" "}
+                          <span>2016-present</span>
+                        </h2>
+                        <ul>
+                          <li>
+                            Meeting with all the team members to discuss and
+                            give an overview of business requirements.
+                          </li>
+                          <li>
+                            {" "}
+                            Upgrading existing legacy application to advanced
+                            MVC application using Prime faces 7, spring web flow
+                            and JSF.
+                          </li>
+                          <li>
+                            Using SVN for Source code Management and merge.
+                          </li>
+                          <li>
+                            Implementing MVC architecture using Spring MVC
+                            framework with JSF framework.
+                          </li>
+                          <li>
+                            Monitoring the error logs using Log4J and fixing the
+                            problems.
+                          </li>
+                          <li>
+                            Using JBoss AS 7.1.1 server in the development
+                            environment.
+                          </li>
+                          <li>
+                            Using IBatis for generating entity classes for ORM.
+                          </li>
+                          <li>
+                            Designing, coding, testing and analyzing software
+                            programs and applications to meet business
+                            requirements.
+                          </li>
+                          <li>
+                            Developing new applications defined and finalized by
+                            business requirements.
+                          </li>
+                          <li>
+                            Maintaining, supporting programs currently in
+                            production and fixing any issues that might appear
+                            based on the priority.
+                          </li>
+                          <li>
+                            Assisting other developers and analysts with
+                            conceptualizing, designing and developing new
+                            software programs and applications.
+                          </li>
+                          <li>
+                            Assisting with the preparation and documentation of
+                            software requirements and specifications.
+                          </li>
+                          <li>
+                            Complying with coding standards, structured
+                            programming techniques, and development processes to
+                            promote maintainability and optimum performance.
+                          </li>
+                          <li>
+                            Performing unit and integration testing according to
+                            standard practices and test plans.
+                          </li>
+                          <li>
+                            Modifying programs to fix documented problems using
+                            standard debugging methods and tools.
+                          </li>
+                          <li>
+                            Maintaining system and program documentation as
+                            changes are made to the code.
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInTop"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Java/J2EE UI Developer at WEX Inc.- Avco Consulting
+                          Inc <span>2014-2016</span>
+                        </h2>
+                        <ul>
+                          <li>
+                            Helped design and implement the Angular Framework
+                            for Spring MVC application.
+                          </li>
+                          <li>
+                            Converted existing old screen which was in JQuery,
+                            JavaScript, CSS, and JSP to Angular Application.
+                          </li>
+                          <li>
+                            Implemented MVC architecture using Spring MVC
+                            framework.
+                          </li>
+                          <li>
+                            Monitoring the error logs using Log4J/Slf4j and
+                            fixing the problems.
+                          </li>
+                          <li>
+                            Wrote Unit Test cases using Mockito test framework.
+                          </li>
+                          <li>
+                            Integrate client side code by consuming web
+                            services.
+                          </li>
+                          <li>
+                            Develop, compile and package application using
+                            Maven.
+                          </li>
+                          <li>
+                            Used Teamcity to build, compile and integrate
+                            application continuously.
+                          </li>
+                          <li>
+                            Used SonarQube to analyze the code for higher
+                            quality and fisheye to do code reviews.
+                          </li>
+                          <li>
+                            Used CVS, SVN for the version management and merge.
+                          </li>
+                          <li>
+                            Trained junior developer to familiarize with the
+                            technology used in Application like Spring MVC,
+                            Maven, JQuery.
+                          </li>
+                          <li>
+                            Used Soapui to test web services and integrate them
+                            to the UI.
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Java Developer at AT & T - Avco Consulting Inc{" "}
+                          <span>2012-2014</span>
+                        </h2>
+                        <ul>
+                          <li>
+                            Involved in high-level design of modules from
+                            Functional Requirement documents.
+                          </li>
+                          <li>
+                            Developed the required Servlets and the JSP pages.
+                          </li>
+                          <li>
+                            Developed UI using HTML, JavaScript, jQuery, CSS,
+                            AJAX and JSP for interactive cross-browser
+                            functionality and complex user interface.
+                          </li>
+                          <li>
+                            Developed various server-side APIs so as to
+                            facilitate communication from the presentation layer
+                            to the servlet layer via asynchronous Ajax requests
+                            to the server side APIs via the built-in jQuery Ajax
+                            capabilities allowing data to be seamlessly loaded
+                            from the server without a browser page refresh.
+                          </li>
+                          <li>
+                            Used Servlets controller to dispatch requests from
+                            JSP pages to business objects.
+                          </li>
+                          <li>
+                            Wrote and fixed Stored Procedures using SQL and
+                            PL/SQL in Toad for Oracle.
+                          </li>
+                          <li>
+                            Created and modified necessary tables in DB as per
+                            new requirement.
+                          </li>
+                          <li>
+                            Helped other developers to achieve their requirement
+                            by explaining the tools and guide them in the right
+                            direction.
+                          </li>
+                          <li>
+                            Separated big Controller logic to multi-controller
+                            Servlet for maintainability, readability, and
+                            simplicity.
+                          </li>
+                          <li>
+                            Communicated with a client to receive/discuss
+                            requirement changes on regular basis.
+                          </li>
+                          <li>
+                            Helped to implement Spring Framework architecture
+                            for re-writing the old application into spring
+                            application by recommending to use a build tool like
+                            Maven with Hibernate to maintain, build, compile and
+                            package software easily.
+                          </li>
+                          <li>
+                            Guided junior developer in need of Technical
+                            difficulties and advised to follow certain
+                            guidelines in terms of improvement.
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Undergrade degree from Salem State University, Salem,
+                          MA <span>2010-2012</span>
+                        </h2>
+                        <p>
+                          I have completed my undergrad education with good GPA
+                          in 2012 as i was always enlisted in Dean's list.
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry begin animate-box"
+                    data-animate-effect="fadeInBottom"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none"></div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
