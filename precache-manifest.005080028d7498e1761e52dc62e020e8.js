1f6940b69d229e4c12799a1557fbfc429c771600
Dinesh Thapa <dinesh-thapa@hotmail.com>
2020-03-22 17:32:14 -0400

    Updates

---
+++ precache-manifest.005080028d7498e1761e52dc62e020e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "22ee1e34853eba56d0ed",
+    "url": "/dinesh-portfolio/static/css/main.38ddf96d.chunk.css"
+  },
+  {
+    "revision": "22ee1e34853eba56d0ed",
+    "url": "/dinesh-portfolio/static/js/main.22ee1e34.chunk.js"
+  },
+  {
+    "revision": "8beb6ddb72d620b73669",
+    "url": "/dinesh-portfolio/static/js/runtime~main.8beb6ddb.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/dinesh-portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "3e51d314a8e20e3aeccafc0d8cd76535",
+    "url": "/dinesh-portfolio/index.html"
+  }
+];
